063c1e68a0d181b0ca0a3c321d4ea037c9319ac7
Kevin Simons <kevin@comoyo.com>
2014-09-04 15:18:57 +0200

    Initial attempt at syntax highlighting
    Current this underlines the entire row where an error occurs and shows
    an icon in the gutter. It also shows the error message in the status bar
    when the cursor is in the row containing the error.

--- climeplugin.py
+++ climeplugin.py
@@ -9,11 +9,12 @@ sys.path.append(lib_path)
 
 import clang.cindex
 
+# globals
+shared_settings = None
+syntax_checker = None
+
 class Settings:
     def __init__(self):
-        self.settings = {}
-
-    def load(self):
         self.settings = sublime.load_settings('Clime.sublime-settings')
 
     def get(self, key_name):
@@ -22,31 +23,94 @@ class Settings:
     def has(self, key_name):
         return self.settings.has(key_name)
 
+class TranslationUnit:
+    def __init__(self, view, index):
+        self.view = view
+        self.index = index
+        self.error_regions = []
+        self.errors_by_line = {}
+        self.translation_unit = self.index.parse(view.file_name())
+        self.calculate_error_regions()
+
+    def reparse(self):
+        self.translation_unit.reparse(unsaved_files=[(self.view.file_name(), self.view.substr(sublime.Region(0, self.view.size())))])
+        self.calculate_error_regions()
+
+    def calculate_error_regions(self):
+        self.error_regions = []
+        self.errors_by_line = {}
+        for diagnostic in self.translation_unit.diagnostics:
+            self.error_regions.append(self.view.full_line(self.view.text_point(diagnostic.location.line - 1, diagnostic.location.column - 1)))
+            self.errors_by_line[diagnostic.location.line - 1] = diagnostic.spelling
+
+    def error_for_line(self, line_number):
+        if line_number in self.errors_by_line:
+            return self.errors_by_line[line_number]
+        return None
+
 class SyntaxChecker:
     def __init__(self):
-        pass
+        self.index = clang.cindex.Index.create()
+        self.translation_units = {}
 
-    def check_syntax(self, view):
-        print('checking stuff now')
-        index = clang.cindex.Index.create()
-        print(index)
+    def create_translation_unit(self, view):
+        if view.id() in self.translation_units or not view.file_name():
+            return
 
-shared_settings = Settings()
-syntax_checker = SyntaxChecker()
+        file_name, file_extension = os.path.splitext(view.file_name())
+        if not file_extension in shared_settings.get('file_extensions'):
+            return
+
+        self.translation_units[view.id()] = TranslationUnit(view, self.index)
+        self.show_diagnostics(view)
+
+    def reparse_translation_unit(self, view):
+        if not view.id() in self.translation_units:
+            return
+
+        self.translation_units[view.id()].reparse()
+        self.show_diagnostics(view)
+
+    def show_diagnostics(self, view):
+        if not view.id() in self.translation_units:
+            return;
+
+        view.erase_regions('clime_errors')
+        error_regions = self.translation_units[view.id()].error_regions
+        if error_regions:
+            view.add_regions('clime_errors', error_regions, 'error', 'cross', sublime.DRAW_SQUIGGLY_UNDERLINE | sublime.DRAW_NO_FILL | sublime.DRAW_NO_OUTLINE | sublime.PERSISTENT)
+            row, col = view.rowcol(view.sel()[0].begin())
+            self.show_status(view, row)
+
+    def show_status(self, view, line_number):
+        if not view.id() in self.translation_units:
+            return;
+
+        error_status = self.translation_units[view.id()].error_for_line(line_number)
+        if (error_status):
+            view.set_status('clime_error', error_status)
+        else:
+            view.erase_status('clime_error')
 
 def plugin_loaded():
-    shared_settings.load()
+    global shared_settings
+    global translation_unit_manager
+    global syntax_checker
 
-    print('plugin_is_loaded' not in globals())
+    shared_settings = Settings()
 
-    if shared_settings.has('libclang_path'):
-        clang.cindex.Config.set_library_path(shared_settings.get('libclang_path'))
-    elif sublime.platform() == 'osx':
-        clang.cindex.Config.set_library_file('/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/libclang.dylib')
+    if not (clang.cindex.Config.library_path or clang.cindex.Config.library_file):
+        if shared_settings.has('libclang_path'):
+            clang.cindex.Config.set_library_path(shared_settings.get('libclang_path'))
+        elif sublime.platform() == 'osx':
+            clang.cindex.Config.set_library_file('/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/libclang.dylib')
+
+    syntax_checker = SyntaxChecker()
 
 class ClimeSyntaxCheckCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        syntax_checker.check_syntax(self.view)
+        syntax_checker.create_translation_unit(self.view)
+        syntax_checker.reparse_translation_unit(self.view)
 
 class ClimeEventListener(sublime_plugin.EventListener):
     def __init__(self):
@@ -56,22 +120,23 @@ class ClimeEventListener(sublime_plugin.EventListener):
         self.run_syntax_check(view)
 
     def on_save_async(self, view):
-        self.run_syntax_check(view)
+        syntax_checker.create_translation_unit(view)
 
     def on_load_async(self, view):
-        self.run_syntax_check(view)
+        syntax_checker.create_translation_unit(view)
+
+    def on_selection_modified_async(self, view):
+        row, col = view.rowcol(view.sel()[0].begin())
+        syntax_checker.show_status(view, row)
 
     def run_syntax_check(self, view):
         if view.id() in self.pendingSyntaxChecks or not view.file_name():
             return
 
         self.pendingSyntaxChecks.add(view.id())
-        sublime.set_timeout(lambda: self.run_syntax_check_callback(view), 3000)
+        sublime.set_timeout(lambda: self.run_syntax_check_callback(view), 1000)
 
     def run_syntax_check_callback(self, view):
         self.pendingSyntaxChecks.discard(view.id())
-
-        file_name, file_extension = os.path.splitext(view.file_name())
-        if file_extension in shared_settings.get('file_extensions'):
-            view.run_command('clime_syntax_check')
+        view.run_command('clime_syntax_check')
 
